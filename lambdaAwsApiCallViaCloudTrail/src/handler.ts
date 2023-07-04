064e5f3d32516ba3df06103bb27dc396c8b859b0
Alex <alex@vontis.nl>
2023-07-04 13:13:48 +0200

    make typing for event

--- lambdaAwsApiCallViaCloudTrail/src/handler.ts
+++ lambdaAwsApiCallViaCloudTrail/src/handler.ts
@@ -1,3 +1,52 @@
-export const handler = async (event: unknown) => {
-  console.log(JSON.stringify(event));
+import { EventBridgeEvent, Handler } from 'aws-lambda';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
+import { CloudTrailRunTaskEvent } from './cloudTrailEvent';
+import { RunTaskCommandInput } from '@aws-sdk/client-ecs';
+
+export const handler: Handler = async (
+  event: EventBridgeEvent<
+    'AWS API Call via CloudTrail',
+    CloudTrailRunTaskEvent
+  >,
+) => {
+  const tasks = [];
+  if (!event['detail']['responseElements']['tasks']) {
+    throw new Error('no tasks found in responseElements');
+  }
+  for (const task of event['detail']['responseElements']['tasks']) {
+    try {
+      await storeInvocationDetails(
+        task['taskArn'] as string,
+        event['detail']['requestParameters'],
+      );
+      tasks.push(task['taskArn']);
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error ? error.message : 'Unknown error';
+      console.error(
+        `Task ${task['taskArn']} could not be saved in dynamodb: ${errorMessage}`,
+      );
+    }
+  }
+  console.log('Logged the following tasks:', tasks);
+};
+
+const storeInvocationDetails = async (
+  taskArn: string,
+  data: RunTaskCommandInput,
+) => {
+  const docClient = DynamoDBDocumentClient.from(new DynamoDBClient({}));
+  const input = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      taskArn: taskArn,
+      ...data,
+      expires:
+        Math.floor(Date.now() / 1000) +
+        parseInt(process.env.TTL_EXPIRES as string),
+    },
+  };
+  const command = new PutCommand(input);
+  await docClient.send(command);
 };
